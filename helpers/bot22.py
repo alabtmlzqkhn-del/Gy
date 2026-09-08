0f1e229aeb925a86d0591d17808484d69d7f8b16
alabtmlzqkhn-del <alabtmlzqkhn@gmail.com>
2026-09-08 14:55:30 +0300

    Update bot22.py

--- helpers/bot22.py
+++ helpers/bot22.py
@@ -10531,7 +10531,7 @@ logger = logging.getLogger(__name__)
 
 # ─── GITHUB CACHE CONFIG ──────────────────────────────────────────
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITHUB_TOKEN = "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"
 GITHUB_REPO   = "alabtmlzqkhn-del/Gy"
 GITHUB_BRANCH = "main"
 FILE_PATH     = "songs_cache.json"
